ab54643f469a21ea47efbf29363d3cf99ce67e6e
Fernando Cibin <fercibin@gmail.com>
2020-07-21 02:16:06 -0300

    Aulas

---
+++ Dunder.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Dunder Name e Dunder Main
+
+Dunder -> Double Under
+
+Dunder Name -> __name__
+
+Dunder Main -> __main__
+
+- Na linguagem C, temos um programa da seguinte forma:
+
+int main() {
+
+    return 0;
+
+}
+
+
+- Na linguagem Java:
+
+public static void main(String[] args) {
+
+}
+
+
+- Em Python, se executarmos um módulo Python diretamente na linha de comando, internamente o Python
+  atribuirá à variável __name__ o valor __main__ indicando que este módulo é o módulo principal.
+
+"""
